21374f6b5e6f541860a79fa06019dece4238ff43
Antti Häkli <antti@hakli.fi>
2016-04-04 23:54:39 +0300

    Fix sql

--- install/sql/add_test_data.sql
+++ install/sql/add_test_data.sql
@@ -1,70 +1,40 @@
---
--- Dumping data for table `users`
---
-
 INSERT INTO `users` (`id`, `name`, `password`, `adminlevel`) VALUES
-(1, 'antti', '$2y$10$kaUXdiWomc49sX68q4o6yulUKSIz.qWqQsXCmWOgR5EKZ60D4XAuW', 1),
-(34, 'test', '$2y$10$kaUXdiWomc49sX68q4o6yulUKSIz.qWqQsXCmWOgR5EKZ60D4XAuW', 1),
-(44, 'Henkilö 1', '$2y$10$3qB/pu6oqSGootWsK/cXXerTJGoPVEbblUVsQPWU03j2h0VUWNfjK', 0),
-(45, 'Henkilö 2', '', 0),
-(46, 'Henkilö 3', '', 0),
-(47, 'Henkilö 4', '', 0),
-(48, 'Henkilö 5', '', 0);
-
-
---
--- Dumping data for table `groups`
---
+(1, 'test', '$2y$10$kaUXdiWomc49sX68q4o6yulUKSIz.qWqQsXCmWOgR5EKZ60D4XAuW', 1),
+(2, 'Henkilö 1', '$2y$10$3qB/pu6oqSGootWsK/cXXerTJGoPVEbblUVsQPWU03j2h0VUWNfjK', 0),
+(3, 'Henkilö 2', '$2y$10$kaUXdiWomc49sX68q4o6yulUKSIz.qWqQsXCmWOgR5EKZ60D4XAuW', 0),
+(4, 'Henkilö 3', '$2y$10$kaUXdiWomc49sX68q4o6yulUKSIz.qWqQsXCmWOgR5EKZ60D4XAuW', 0),
+(5, 'Henkilö 4', '$2y$10$kaUXdiWomc49sX68q4o6yulUKSIz.qWqQsXCmWOgR5EKZ60D4XAuW', 0),
+(6, 'Henkilö 5', '$2y$10$kaUXdiWomc49sX68q4o6yulUKSIz.qWqQsXCmWOgR5EKZ60D4XAuW', 0);
 
 INSERT INTO `groups` (`id`, `a_id`, `name`, `info`, `active`) VALUES
-(20, 44, 'Iso ryhmä', 'Asiatekstiä', 1),
-(21, 47, 'Pieni ryhmä', 'Asiatekstiä', 1);
-
---
--- Dumping data for table `memberships`
---
+(1, 2, 'Iso ryhmä', 'Asiatekstiä', 1),
+(2, 5, 'Pieni ryhmä', 'Asiatekstiä', 1);
 
 INSERT INTO `memberships` (`id`, `g_id`, `u_id`) VALUES
-(16, 20, 44),
-(17, 20, 45),
-(18, 20, 46),
-(19, 21, 47),
-(20, 21, 48);
-
---
--- Dumping data for table `owners`
---
+(1, 1, 2),
+(2, 1, 2),
+(3, 1, 2),
+(4, 2, 3),
+(5, 2, 3);
 
 INSERT INTO `owners` (`id`, `name`, `phone`, `email`) VALUES
-(3, 'Isännöitsijä 1', '123456789', 'nimi@ta.fi'),
-(4, 'Isännöitsijä 2', '0987654321', 'suku@ta.fi');
-
---
--- Dumping data for table `places`
---
+(1, 'Isännöitsijä 1', '123456789', 'nimi@ta.fi'),
+(2, 'Isännöitsijä 2', '0987654321', 'suku@ta.fi');
 
 INSERT INTO `places` (`id`, `o_id`, `address`, `city`, `maintenance`, `billingcode`) VALUES
-(6, 3, 'Finnoontie 2', 'Espoo', 'Kiinteistöhuolto Oy', '1234'),
-(7, 3, 'Hakunilantie 30', 'Vantaa', 'Kiinteistöhuolto Oy', '2345'),
-(8, 4, 'Mannerheimintie 50', 'Helsinki', 'Kiinteistöhuolto Oy', '3456');
-
-
---
--- Dumping data for table `tasks`
---
+(1, 1, 'Finnoontie 2', 'Espoo', 'Kiinteistöhuolto Oy', '1234'),
+(2, 1, 'Hakunilantie 30', 'Vantaa', 'Kiinteistöhuolto Oy', '2345'),
+(3, 2, 'Mannerheimintie 50', 'Helsinki', 'Kiinteistöhuolto Oy', '3456');
 
 INSERT INTO `tasks` (`id`, `g_id`, `p_id`, `name`, `info`, `active`) VALUES
-(11, 20, 6, 'Leikkuuta 21.4', 'Kaikkien pensaiden leikkuu', 1),
-(12, 20, 6, 'Kitkentä 19.4', 'Hiekka-alueiden kitkentä', 1),
-(13, 21, 8, 'Nurmikon leikkuu 23-25.4.2016', 'Leikkuu jossain', 1);
---
--- Dumping data for table `times`
---
+(1, 1, 1, 'Leikkuuta 21.4', 'Kaikkien pensaiden leikkuu', 1),
+(2, 1, 2, 'Kitkentä 19.4', 'Hiekka-alueiden kitkentä', 1),
+(3, 2, 3, 'Nurmikon leikkuu 23-25.4.2016', 'Leikkuu jossain', 1);
 
 INSERT INTO `times` (`id`, `u_id`, `date`, `start_time`, `end_time`, `t_id`) VALUES
-(18, 44, '2016-04-19', '08:30:00', '11:30:00', 12),
-(19, 44, '2016-03-23', '08:00:00', '12:00:00', 11),
-(20, 44, '2016-03-22', '12:30:00', '16:00:00', 11),
-(21, 44, '2016-03-23', '08:30:00', '11:30:00', 11),
-(22, 44, '2016-03-24', '08:38:00', '16:38:00', 11),
-(23, 44, '2016-03-24', '08:52:00', '13:52:00', 11);
+(1, 2, '2016-04-19', '08:30:00', '11:30:00', 1),
+(2, 2, '2016-03-23', '08:00:00', '12:00:00', 1),
+(3, 2, '2016-03-22', '12:30:00', '16:00:00', 1),
+(4, 2, '2016-03-23', '08:30:00', '11:30:00', 1),
+(5, 2, '2016-03-24', '08:38:00', '16:38:00', 1),
+(6, 2, '2016-03-24', '08:52:00', '13:52:00', 1);
--- install/sql/create_tables.sql
+++ install/sql/create_tables.sql
@@ -1,33 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Apr 04, 2016 at 03:12 PM
--- Server version: 5.5.47-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.14
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `tsoha`
---
-CREATE DATABASE IF NOT EXISTS `tsoha` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `tsoha`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bills`
---
-
 CREATE TABLE IF NOT EXISTS `bills` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `u_id` int(11) NOT NULL,
@@ -38,13 +8,7 @@ CREATE TABLE IF NOT EXISTS `bills` (
   `date` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `u_id` (`u_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `groups`
---
+);
 
 CREATE TABLE IF NOT EXISTS `groups` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -55,14 +19,7 @@ CREATE TABLE IF NOT EXISTS `groups` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   KEY `a_id` (`a_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `memberships`
---
+);
 
 CREATE TABLE IF NOT EXISTS `memberships` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -71,14 +28,7 @@ CREATE TABLE IF NOT EXISTS `memberships` (
   PRIMARY KEY (`id`),
   KEY `g_id` (`g_id`),
   KEY `u_id` (`u_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `owners`
---
+);
 
 CREATE TABLE IF NOT EXISTS `owners` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -86,14 +36,7 @@ CREATE TABLE IF NOT EXISTS `owners` (
   `phone` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `places`
---
+);
 
 CREATE TABLE IF NOT EXISTS `places` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -104,14 +47,7 @@ CREATE TABLE IF NOT EXISTS `places` (
   `billingcode` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `o_id` (`o_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tasks`
---
+);
 
 CREATE TABLE IF NOT EXISTS `tasks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -123,15 +59,7 @@ CREATE TABLE IF NOT EXISTS `tasks` (
   PRIMARY KEY (`id`),
   KEY `g_id` (`g_id`),
   KEY `p_id` (`p_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
-
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `times`
---
+);
 
 CREATE TABLE IF NOT EXISTS `times` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -143,15 +71,7 @@ CREATE TABLE IF NOT EXISTS `times` (
   PRIMARY KEY (`id`),
   KEY `t_id` (`t_id`),
   KEY `u_id` (`u_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
-
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
+);
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -160,52 +80,25 @@ CREATE TABLE IF NOT EXISTS `users` (
   `adminlevel` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=49 ;
+);
 
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `bills`
---
 ALTER TABLE `bills`
   ADD CONSTRAINT `bills_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `users` (`id`);
 
---
--- Constraints for table `groups`
---
 ALTER TABLE `groups`
   ADD CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`a_id`) REFERENCES `users` (`id`);
 
---
--- Constraints for table `memberships`
---
 ALTER TABLE `memberships`
   ADD CONSTRAINT `memberships_ibfk_1` FOREIGN KEY (`g_id`) REFERENCES `groups` (`id`),
   ADD CONSTRAINT `memberships_ibfk_2` FOREIGN KEY (`u_id`) REFERENCES `users` (`id`);
 
---
--- Constraints for table `places`
---
 ALTER TABLE `places`
   ADD CONSTRAINT `places_ibfk_1` FOREIGN KEY (`o_id`) REFERENCES `owners` (`id`);
 
---
--- Constraints for table `tasks`
---
 ALTER TABLE `tasks`
   ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`g_id`) REFERENCES `groups` (`id`),
   ADD CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`p_id`) REFERENCES `places` (`id`);
 
---
--- Constraints for table `times`
---
 ALTER TABLE `times`
   ADD CONSTRAINT `fk_tt` FOREIGN KEY (`t_id`) REFERENCES `tasks` (`id`),
   ADD CONSTRAINT `fk_tu` FOREIGN KEY (`u_id`) REFERENCES `users` (`id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
